9245f2dad4c04b885da64d96c9706fd5a0833a61
JCS <joshsad2@gmail.com>
2017-09-25 23:21:22 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -6,26 +6,26 @@ function getIt() {
   })
 }
 
-// function frameIt() {
-//   $('img').on('load', function() {
-//     $('img').addClass('tasty')
-//   })
-// }
-//
-// function pressIt() {
-//   $('#typing').on('keydown', function(e) {
-//     // 'g' corresponds to 71
-//     if (e.which === 71) {
-//       alert("You pressed 'g'!")
-//     }
-//   })
-// }
-//
-// function submitIt() {
-//   $('form').on('submit', function(e) {
-//     alert('Your form is going to be submitted now.')
-//   })
-// }
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(e) {
+    // 'g' corresponds to 71
+    if (e.which === 71) {
+      alert("You pressed 'g'!")
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function(e) {
+    alert('Your form is going to be submitted now.')
+  })
+}
 
 $(document).ready(function(){
 
